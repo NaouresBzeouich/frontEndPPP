acc6529b4998a06d52cf257825d668e955e39db9
NaouresBzeouich <naoures.bzeouich@insat.ucar.tn>
2024-06-01 14:02:26 +0100

    add google gemini effect

--- ppp/src/components/bodyParts/Btn.tsx
+++ ppp/src/components/bodyParts/Btn.tsx
@@ -1,7 +1,7 @@
 import { alpha } from "@mui/material";
-import React from 'react';
-import Button from '@mui/material/Button';
-import { Typography , Box } from '@mui/material';
+import React from "react";
+import Button from "@mui/material/Button";
+import { Typography, Box } from "@mui/material";
 
 interface StartBtnProps {
   text: string;
@@ -10,15 +10,17 @@ interface StartBtnProps {
 
 const StartBtn: React.FC<StartBtnProps> = ({ text, onClick }) => {
   return (
-    <Box sx={(theme) => ({
-      boxShadow:
+    <Box
+      sx={(theme) => ({
+        boxShadow:
           theme.palette.mode === "light"
             ? `0 0 12px 8px ${alpha("#9CCCFC", 0.2)}`
             : `0 0 24px 12px ${alpha("#033363", 0.2)}`,
-          })}>
-    <Button variant="outlined"size="large" onClick={onClick} >
-      <Typography variant="h5">{text}</Typography>
-    </Button>
+      })}
+    >
+      <Button variant="contained" size="large" onClick={onClick}>
+        <Typography variant="h5">{text}</Typography>
+      </Button>
     </Box>
   );
 };
--- ppp/src/components/pageParts/Body.tsx
+++ ppp/src/components/pageParts/Body.tsx
@@ -1,12 +1,27 @@
+"use client";
 import * as React from "react";
 import { alpha, Box, Container, Stack } from "@mui/material";
 import Title from "../bodyParts/Title";
 import Btn from "../bodyParts/Btn";
 import BodyContainer from "../bodyParts/Container";
+import { useScroll, useTransform } from "framer-motion";
+import { GoogleGeminiEffect } from "../ui/Google-Gemini-effect";  
 
 const Body: React.FC = () => {
   const [step, setStep] = React.useState<number>(0);
   const [uploadedImage, setUploadedImage] = React.useState<string | null>(null);
+
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
 
   const handleButtonClick = () => {
     if (step === 0) {
@@ -33,45 +48,70 @@ const Body: React.FC = () => {
     setUploadedImage(imageData);
   };
 
-
   return (
-    <Box
-      id="Body"
-      sx={(theme) => ({
-        width: "100%",
-        backgroundImage:
-          theme.palette.mode === "light"
-            ? "linear-gradient(180deg, #CEE5FD, #FFF)"
-            : `linear-gradient(#02294F, ${alpha("#090E10", 0.0)})`,
-        backgroundSize: "100% 20%",
-        backgroundRepeat: "no-repeat",
-      })}
-    >
-      <Container
+    <div style={{ position: 'relative' }}>
+      <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          pt: { xs: 14, sm: 20 },
-          pb: { xs: 8, sm: 12 },
+          position: 'absolute',
+          top: -1,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: 1,
         }}
       >
-        <Stack spacing={2} useFlexGap sx={{ width: { xs: "100%", sm: "70%" } }}>
-          <Title />
+        <GoogleGeminiEffect
+          pathLengths={[
+            pathLengthFirst,
+            pathLengthSecond,
+            pathLengthThird,
+            pathLengthFourth,
+            pathLengthFifth,
+          ]}
+        />
+      </Box>
+      
+      <Box
+        id="Body"
+        sx={(theme) => ({
+          position: 'relative',
+          zIndex: 2,
+          width: "100%",
+          backgroundImage:
+            theme.palette.mode === "light"
+              ? "linear-gradient(180deg, #CEE5FD, #FFF)"
+              : `linear-gradient(#02294F, ${alpha("#090E10", 0.0)})`,
+          backgroundSize: "100% 20%",
+          backgroundRepeat: "no-repeat",
+        })}
+      >
+        <Container
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            pt: 30,
+          }}
+        >
+          <Stack spacing={6} useFlexGap sx={{ width: "100%"}}>
+            <Title />
 
-          <Stack
-            direction={{ xs: "column", sm: "row" }}
-            alignSelf="center"
-            spacing={1}
-            useFlexGap
-            sx={{ pt: 2, width: { xs: "100%", sm: "auto" } }}
-          >
-            <Btn text={getButtonLabel()} onClick={handleButtonClick} />
+            <Stack
+              direction={{ xs: "column", sm: "row" }}
+              alignSelf="center"
+              useFlexGap
+              sx={{ pt: 2, width: { xs: "100%", sm: "auto" } }}
+            >
+              <Btn text={getButtonLabel()} onClick={handleButtonClick} />
+            </Stack>
           </Stack>
-        </Stack>
-        <BodyContainer text={getButtonLabel()} onUpload={handleImageUpload}  clearImage={step === 0}/>
-      </Container>
-    </Box>
+          <br/>
+          <br/>
+          <br/>
+          <BodyContainer text={getButtonLabel()} onUpload={handleImageUpload} clearImage={step === 0}/>
+        </Container>
+      </Box>
+    </div>
   );
 };
 
